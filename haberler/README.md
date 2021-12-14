591039f9a3df841e5a8201d068ad63c2f1a40ddc
SamettEce <96135911+SamettEce@users.noreply.github.com>
2021-12-14 19:56:20 +0300

    vize

--- haberler/README.md
+++
@@ -1,3 +0,0 @@
-# Flutter Tutorials on My [Youtube Channel](https://www.youtube.com/user/aisV25)
-
-!["Youtube Channel"](channel.png)
--- haberler/todoApp/android/app/src/main/kotlin/com/example/todoApp/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.todoApp
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
